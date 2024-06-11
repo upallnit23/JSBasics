ba1de5c09db296a84a8f82c9216c5956896322d4
upallnit23 <cher_cole@email.com>
2024-06-11 12:58:16 -0500

    adding lesson 1 from javascript

---
+++ jsbasics.js
@@ -0,0 +1,14 @@
+/* Understanding Data Types in JavaScript
+Tasks 1 thru 3 */
+
+let name1 = "Carlissa Riley"
+let age = 19
+let grade = "A"
+let attendance = true
+
+console.log("Name", name1)
+console.log("Age", age)
+console.log("Grade", grade)
+console.log("Attendance", attendance)
+
+
---
+++ jsbasics2.js
@@ -0,0 +1,38 @@
+/*  Exploring JavaScript Operators  */
+
+let a = 20;
+let b = 5;
+let a1 = 44;
+let b1 = 2;
+let sum = a + b;
+let diff = a - b;
+let product = a * b;
+let quotient = a / b;
+
+let sum1 = a1 + b1;
+let diff1 = a1 - b1;
+let product1 = a1 * b1;
+let quotient1 = a1 / b1;
+
+console.log("Sum", sum);
+console.log("Difference", diff);
+console.log("Product", product);
+console.log("Quotient", quotient);
+
+console.log("Sum", sum1);
+console.log("Difference", diff1);
+console.log("Product", product1);
+console.log("Quotient", quotient1);
+
+let isGreaterThan = a1 > b1;
+let isEqualTo = a1 == b1;
+let notEqualTo = a1!= b1;
+
+console.log("Is num1 equal to num2?", isEqualTo);
+console.log("Is num1 greater than num2?", isGreaterThan);
+console.log("Is num1 not equal to num2?", notEqualTo);
+
+let isPositive = a1 > b1;
+let isEven = a1;
+console.log("Are both numbers positive?", isPositive);
+console.log("Is at least one number even?", isEven);
